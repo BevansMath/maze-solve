a6bbab44ca9d22611bb1c68ab4f3178e54341d5c
Brandon <6256162+BevansMath@users.noreply.github.com>
2022-05-26 10:16:22 -0500

    Added program dependent files

---
+++ tests.py
@@ -0,0 +1,44 @@
+import unittest
+
+from maze import Maze
+
+class Test:
+    def test_maze_creates_cells(self): #Creates_cells() method creates the correct number of cells
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0,0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    def test_maze_remove_walls(self):
+        num_cols = 12
+        num_rows = 10
+        m2 = Maze(0,0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m2._cells[0][0].has_top),
+            False,
+        )
+        self.assertEqual(
+            len(m2._cells[self._num_rows - 1][self._num_cols - 1].has_bottom,
+            False,)
+        )
+    
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0,0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                    
+                )
+if __name__ == "__main__": # Driver function to test main script
+    unittest.main()
